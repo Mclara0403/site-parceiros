52a60d0abe918a41b49aaec77128f8ee93d7f7bd
Mclara0403 <maria.ferreira.5@academico.ifpb.edu.br>
2026-05-27 11:37:44 -0300

    cod.py

---
+++ cod.py
@@ -0,0 +1,18 @@
+import streamlit as at
+st.title("Empresas Parceiras")
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.image("mc-logo.png", use_container_width=True)
+  st.title("MC Donald's ")
+  st.link_button("Acessar","https://www.mcdonalds.com.br/")
+  st.write("Maior empresa de fast food do mundo ")
+with col2:
+  st.image("netflix-logo.jpg", use_container_width=True)
+  st.title("Netflix")
+  st.link_button("Acessar","https://www.netflix.com/br/")
+  st.write("Maior empresa de entretenimento do mundo")
+with col3:
+  st.image("nike-logo.png", use_container_width=True)
+  st.title("Nike ")
+  st.link_button("Acessar","http://nike.com.br/")
+  st.write("Maior empresa de artigos esportivos do mundo ")
